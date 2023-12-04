ba435332c4e959e8ee552ec8aa0df9dc5ece1254
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-04 15:10:25 +0300

    Product Controller

---
+++ Restore.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restore.API.Data;
+using Restore.API.Entities;
+
+namespace Restore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return Ok( await context.Products.FindAsync(id));
+        }
+    }
+}
