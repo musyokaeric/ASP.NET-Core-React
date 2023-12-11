07d30c9e97f97368947040fa427a21c3a2eca510
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-11 09:23:59 +0300

    Using useState() and useEffect() to pull data from API

--- restore.client/src/App.tsx
+++ restore.client/src/App.tsx
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+    const [products, setProducts] = useState([
+        { name: 'product1', price: 100.00 },
+        { name: 'product2', price: 100.00 },
+    ]);
+
+    useEffect(() => {
+        fetch('https://localhost:7263/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data));
+    }, [])
+
+    function addProduct() {
+        setProducts([...products, { name: 'product' + (products.length + 1), price: 100.00 + (products.length * 100) }])
+    }
 
     return (
         <>
             <h1>Re-Store</h1>
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>{product.name} - {product.price}</li>
+                ))}
+            </ul>
+            <button onClick={addProduct}>Add Product</button>
         </>
     )
 }
