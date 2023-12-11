5d7729f1b492fc30c593a4d10a5072e371fdd4b4
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-11 10:17:10 +0300

    File and folder organisation

--- restore.client/src/app/layout/App.tsx
+++ restore.client/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import './App.css'
-import { Product } from './product'
+import '../../App.css'
+import { Product } from '../models/product'
 
 function App() {
     const [products, setProducts] = useState<Product[]>([]);
