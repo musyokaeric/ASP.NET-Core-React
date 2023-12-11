e4d0a7feef03bbc35de091fb51db217f732be001
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-11 13:57:00 +0300

    Material UI - Adding Cards for our Products

--- restore.client/src/features/catalog/ProductCard.tsx
+++ restore.client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardMedia, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -7,11 +7,32 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
     return (
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.imageUrl} />
-            </ListItemAvatar>
-            <ListItemText>{product.name} - {product.price}</ListItemText>
-        </ListItem>
+        //<ListItem key={product.id}>
+        //    <ListItemAvatar>
+        //        <Avatar src={product.imageUrl} />
+        //    </ListItemAvatar>
+        //    <ListItemText>{product.name} - {product.price}</ListItemText>
+        //</ListItem>
+
+        <Card>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="https://picsum.photos/id/20/367/267"
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     )
 }
