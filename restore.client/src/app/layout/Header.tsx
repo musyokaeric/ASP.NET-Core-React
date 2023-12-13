71809b9ced4ffa269cedd272e288956ca6e586b9
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-13 14:32:49 +0300

    Adding Nav Links to the AppBar

--- restore.client/src/app/layout/Header.tsx
+++ restore.client/src/app/layout/Header.tsx
@@ -1,6 +1,30 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from '@mui/material';
+import { Brightness4, Brightness7, ShoppingCart } from '@mui/icons-material/';
+//import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { NavLink } from 'react-router-dom';
+
+const midLinks = [
+    { title: 'Catalog', path: '/catalog' },
+    { title: 'About', path: '/about' },
+    { title: 'Contact', path: '/contact' },
+];
+
+const rightLinks = [
+    { title: 'Login', path: '/login' },
+    { title: 'Register', path: '/register' },
+];
+
+const navStyles = {
+    color: 'inherit',
+    typography: 'h6',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    },
+};
 
 interface Props {
     darkMode: boolean;
@@ -10,13 +34,47 @@ interface Props {
 export default function Header({ darkMode, handleThemeChange }: Props) {
     return (
         <AppBar position="static" sx={{ mb: 4 }}>
-            <Toolbar>
-                <Typography variant="h6">
-                    Re-Store 
-                </Typography>
-                <IconButton sx={{ ml: 1 }} onClick={handleThemeChange} color="inherit">
-                    {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-                </IconButton>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display='flex' alignItems='center'>
+                    <Typography variant="h6" component={NavLink} to='/' sx={{ color: 'inherit', textDecoration: 'none' }}>
+                        Re-Store
+                    </Typography>
+                    <IconButton sx={{ ml: 1 }} onClick={handleThemeChange} color="inherit">
+                        {darkMode ? <Brightness7 /> : <Brightness4 />}
+                    </IconButton>
+                </Box>
+
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyles}>
+                            {title}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }} >
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}>
+                                {title}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     );
