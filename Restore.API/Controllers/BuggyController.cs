554c85c6d685ef1d873ff8c8dfb9454b6f9713f9
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-14 16:32:22 +0300

    Simple error controller

---
+++ Restore.API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restore.API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ProblemDetails { Title = "This is a bad request" });
+        }
+
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem1", "This is the first error");
+            ModelState.AddModelError("Problem2", "This is the second error");
+
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+    }
+}
--- Restore.API/Controllers/ProductsController.cs
+++ Restore.API/Controllers/ProductsController.cs
@@ -6,9 +6,7 @@ using Restore.API.Entities;
 
 namespace Restore.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext context;
 
