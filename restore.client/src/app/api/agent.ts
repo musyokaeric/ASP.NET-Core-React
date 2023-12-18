248931a9c2b78b8809613a65835ee9df389a21aa
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-18 17:25:56 +0300

    Handling validation errors

--- restore.client/src/app/api/agent.ts
+++ restore.client/src/app/api/agent.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 
 axios.defaults.baseURL = "https://localhost:7263/api/";
 
@@ -7,7 +8,29 @@ const responseBody = (response: AxiosResponse) => response.data;
 axios.interceptors.response.use(response => {
     return response;
 }, (error: AxiosError) => {
-    console.log('caught by interceptor');
+    const { data, status } = error.response as AxiosResponse;
+    switch (status) {
+        case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 500:
+            toast.error(data.title);
+            break;
+        default:
+            break;
+    }
     return Promise.reject(error.response);
 })
 
--- restore.client/src/app/layout/App.tsx
+++ restore.client/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import Catalog from "../../features/catalog/Catalog";
 import Header from './Header';
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { Outlet } from "react-router";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -22,10 +23,11 @@ function App() {
 
     return (
         <ThemeProvider theme={theme}>
+            <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
             <CssBaseline />
             <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
             <Container>
-                <Outlet/>
+                <Outlet />
             </Container>
 
         </ThemeProvider>
