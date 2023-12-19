52edc1fe8c7098e95bb6ce68abf2341e94ffcc14
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-19 15:21:19 +0300

    Created Basket Entity

---
+++ Restore.API/Entities/BasketItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restore.API.Entities
+{
+    [Table("BasketItems")]
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        // navigation properties
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+    }
+}
