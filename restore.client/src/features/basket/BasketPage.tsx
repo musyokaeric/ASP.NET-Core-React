fd4438b6c2007ad72c1ade49cd2f3b263382ea17
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-12-19 18:57:32 +0300

    Styling the basket items

--- restore.client/src/features/basket/BasketPage.tsx
+++ restore.client/src/features/basket/BasketPage.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { Basket } from "../../app/models/basket";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Typography } from "@mui/material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
 export default function BasketPage() {
     const [loading, setLoading] = useState(true);
@@ -20,6 +21,38 @@ export default function BasketPage() {
     if (!basket) return <Typography variant='h3'>Your basket is empty</Typography>
 
     return (
-        <h1>Buyer Id = {basket.buyerId}</h1>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">Subtotal</TableCell>
+                        <TableCell align="right"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {basket.items.map((item) => (
+                        <TableRow
+                            key={item.productId}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {item.name}
+                            </TableCell>
+                            <TableCell align="right">${item.price.toFixed(2)}</TableCell>
+                            <TableCell align="right">{item.quantity}</TableCell>
+                            <TableCell align="right">${(item.price * item.quantity).toFixed(2)}</TableCell>
+                            <TableCell align="right">
+                                <IconButton color='error'>
+                                    <Delete />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
