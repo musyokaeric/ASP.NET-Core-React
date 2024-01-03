79746a1ebe55dfa710950c3ee98ac5e652ec7754
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-01-03 14:44:32 +0300

    Added Currency Format Utility

--- restore.client/src/features/catalog/ProductCard.tsx
+++ restore.client/src/features/catalog/ProductCard.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/util/util";
 
 interface Props {
     product: Product;
@@ -18,8 +19,8 @@ export default function ProductCard({ product }: Props) {
     function handleAddItem(productId: number) {
         setLoading(true);
         agent.Basket.addItem(productId)
-            .then(basket => setBasket(basket))
-            .catch(error => console.log(error))
+            .then((basket) => setBasket(basket))
+            .catch((error) => console.log(error))
             .finally(() => setLoading(false));
     }
 
@@ -33,34 +34,30 @@ export default function ProductCard({ product }: Props) {
 
         <Card>
             <CardHeader
-                avatar={
-                    <Avatar sx={{ bgcolor: 'secondary.main' }}>
-                        {product.name.charAt(0).toUpperCase()}
-                    </Avatar>
-                }
+                avatar={<Avatar sx={{ bgcolor: "secondary.main" }}>{product.name.charAt(0).toUpperCase()}</Avatar>}
                 title={product.name}
                 titleTypographyProps={{
-                    sx: { fontWeight: 'bold', color: 'primary.main' }
+                    sx: { fontWeight: "bold", color: "primary.main" },
                 }}
             />
 
-            <CardMedia
-                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
-                image={product.imageUrl}
-                title={product.name}
-            />
+            <CardMedia sx={{ height: 140, backgroundSize: "contain", bgcolor: "primary.light" }} image={product.imageUrl} title={product.name} />
             <CardContent>
-                <Typography gutterBottom color='secondary' variant="h5">
-                    ${product.price.toFixed(2)}
+                <Typography gutterBottom color="secondary" variant="h5">
+                    {currencyFormat(product.price)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {product.brand} / {product.type}
                 </Typography>
             </CardContent>
             <CardActions>
-                <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
-                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
+                <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">
+                    Add to cart
+                </LoadingButton>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">
+                    View
+                </Button>
             </CardActions>
         </Card>
-    )
+    );
 }
