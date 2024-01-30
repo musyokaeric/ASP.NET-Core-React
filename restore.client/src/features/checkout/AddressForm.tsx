9155d568671ad63b2af43afe62a8919fde17c41d
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-01-30 17:31:03 +0300

    Using react hook form context to maintain shipping address state

--- restore.client/src/features/checkout/AddressForm.tsx
+++ restore.client/src/features/checkout/AddressForm.tsx
@@ -1,44 +1,41 @@
-import { Typography, Grid, TextField, FormControlLabel, Checkbox, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { Typography, Grid, FormControlLabel, Checkbox } from "@mui/material";
+import { useFormContext } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 
 export default function AddressForm() {
-    const { control, handleSubmit } = useForm();
+    const { control } = useFormContext();
 
     return (
         <>
             <Typography variant='h6' gutterBottom>
                 Shipping address
             </Typography>
-            <form onSubmit={handleSubmit((data) => console.log(data))}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={12}>
-                        <AppTextInput control={control} name='fullName' label='Full name' />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control} name='address1' label='Address 1' />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control} name='address2' label='Address 2' />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control} name='city' label='City' />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control} name='state' label='State' />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control} name='zip' label='Zip/Postal code' />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control} name='region' label='Country' />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControlLabel control={<Checkbox color='secondary' name='saveAddress' value='yes' />} label='Use this address for payment details' />
-                    </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12}>
+                    <AppTextInput control={control} name='fullName' label='Full name' />
                 </Grid>
-                <Button type='submit'>Submit form</Button>
-            </form>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='address1' label='Address 1' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='address2' label='Address 2' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='city' label='City' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='state' label='State' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='zip' label='Zip/Postal code' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='region' label='Country' />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControlLabel control={<Checkbox color='secondary' name='saveAddress' value='yes' />} label='Use this address for payment details' />
+                </Grid>
+            </Grid>
         </>
     );
 }
